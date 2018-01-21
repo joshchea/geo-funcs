121a8a5ffc6fa5d8e56b274d95fafe36fdb23d7e
Chetan Joshi <joshchea@vt.edu>
2018-01-21 11:19:12 -0800

    Update compute_gcd.py

--- scripts/compute_gcd.py
+++ scripts/compute_gcd.py
@@ -6,7 +6,7 @@
 # Dependencies:None
 # Created:     01/20/2018
 #              
-# Copyright:   (c) Chetan Joshi 2015
+# Copyright:   (c) Chetan Joshi 2018
 # Licence:     Permission is hereby granted, free of charge, to any person obtaining a copy
 #              of this software and associated documentation files (the "Software"), to deal
 #              in the Software without restriction, including without limitation the rights
